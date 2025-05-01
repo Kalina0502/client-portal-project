c61f6de1e0c1963ddf71ffc86c6ecf5ed8aeaa85
Kalina0502 <kkj.04b14@gmail.com>
2025-05-01 20:20:43 +0300

    chore: use environment variable for PORT and clean up comments

--- server/server.js
+++ server/server.js
@@ -3,14 +3,10 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-//const PORT = 3000;
 const PORT = process.env.PORT || 3000;
 
 // 1. Сервиране на статичните файлове от папка "public"
 app.use(express.static(path.join(__dirname, '..', 'public')));
-// Това ще направи съдържанието на /public достъпно по HTTP,
-// например dashboard.html ще е достъпен на http://localhost:3000/dashboard.html
-// (името на статичната папка не влиза в URL-то)&#8203;:contentReference[oaicite:4]{index=4}.
 
 // 2. API маршрут за връщане на съдържанието на data.json
 app.get('/api/data', (req, res) => {
